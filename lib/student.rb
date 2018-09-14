22f7ef1400880a083f02e327df11ec95d1277c42
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 00:33:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,7 +50,7 @@ class Student
   def self.find_by_name(name)
     student = DB[:conn].execute("SELECT * FROM students WHERE name = ?", name)
     self.new_from_db(student)
-    self
+    
   end
   
   def update
