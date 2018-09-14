e0cd9ad60a0277505b3204ccde941b6ccc6e66ad
Arvida Jimenez <arvida.jimenez@gmail.com>
2018-09-14 00:40:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,11 +44,9 @@ class Student
   end
   
   def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
     
-    self.new(id, name, grade)
+    
+    self.new(id = row[0], name = row[1], grade = row[2])
   end
   
   def self.find_by_name(name)
